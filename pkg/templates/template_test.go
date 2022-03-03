2d9fdee60b71800a7cd21ab4d3e6fcfdfdd7d557
huiwq1990 <huiwq1990@163.com>
2022-03-03 17:24:06 +0800

    only tolerate specify key

--- pkg/templates/template_test.go
+++ pkg/templates/template_test.go
@@ -17,9 +17,15 @@ limitations under the License.
 package templates
 
 import (
+	"fmt"
 	"reflect"
+	"sort"
 	"testing"
 
+	"github.com/openyurtio/yurtcluster-operator/pkg/projectinfo"
+	"github.com/openyurtio/yurtcluster-operator/pkg/templates/yurttunnel"
+	"github.com/openyurtio/yurtcluster-operator/pkg/util"
+	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -78,5 +84,65 @@ func TestMergeExtraArgsForContainer(t *testing.T) {
 				t.Errorf("expect command %v, but got %v", tc.expected.Command, tc.container.Command)
 			}
 		})
+	}
+}
+
+func TestAgentWithExtraArgs(t *testing.T) {
+	obj, err := util.YamlToObject([]byte(yurttunnel.TemplateContent))
+	if err != nil {
+		t.Fatalf("load template fail")
+	}
+	template := obj.(*corev1.ConfigMap)
+
+	dataKey := yurttunnel.AgentDaemonSet
+
+	values := map[string]string{
+		"edgeNodeLabel":        projectinfo.GetEdgeWorkerLabelKey(),
+		"yurtTunnelAgentImage": "v0.0.1",
+	}
+
+	tunnelserverAddr := "tunnelserver-addr"
+	tunnelserverAddrVal := "127.0.0.1:31713"
+	extraArgs := map[string]string{
+		tunnelserverAddr: tunnelserverAddrVal,
+	}
+
+	yamlContent, ok := template.Data[dataKey]
+	if !ok {
+		t.Fatalf("get data fail. key: %s", dataKey)
+	}
+
+	renderContent, err := util.RenderTemplate(yamlContent, values)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	agentYaml, err := MergeExtraArgs(renderContent, extraArgs, yurttunnel.AgentContainerName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	agentObj, err := util.YamlToObject([]byte(agentYaml))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedArgs := []string{
+		"--node-name=$(NODE_NAME)",
+		"--node-ip=$(POD_IP)",
+		"--v=2",
+		fmt.Sprintf("--%s=%s", tunnelserverAddr, tunnelserverAddrVal),
+	}
+
+	agentDs := agentObj.(*v1.DaemonSet)
+	for i := range agentDs.Spec.Template.Spec.Containers {
+		container := agentDs.Spec.Template.Spec.Containers[i]
+		if container.Name == yurttunnel.AgentContainerName {
+			sort.Strings(container.Args)
+			sort.Strings(expectedArgs)
+			if !reflect.DeepEqual(container.Args, expectedArgs) {
+				t.Errorf("args not equal")
+			}
+		}
 	}
 }
